483306bebaeaf9f55dad04f9b3f10df614d6c248
Jennifer Coy <jjcoy@anderson.edu>
2020-09-20 20:25:53 -0400

    there is one paddle

--- frontend/src/components/GameArea.js
+++ frontend/src/components/GameArea.js
@@ -4,11 +4,18 @@
 // standard react components
 import React from 'react';
 
+// import paddles
+import Paddle from './Paddle';
+
 // style for this page
 import './GameArea.css';
 
 const GameArea = () => {
-  return <div className="GameArea"></div>;
+  return (
+    <div className="GameArea">
+      <Paddle />
+    </div>
+  );
 };
 
 export default GameArea;
---
+++ frontend/src/components/Paddle.js
@@ -0,0 +1,14 @@
+// src/components/Paddle.js
+// the Game area for Pong
+
+// standard react components
+import React from 'react';
+
+// style for this page
+import './Paddle.css';
+
+const Paddle = () => {
+  return <div className="Paddle"></div>;
+};
+
+export default Paddle;
